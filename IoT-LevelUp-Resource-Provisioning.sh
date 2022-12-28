cb5d478d42b7fb69ae76725d325e948999775afd
Gordon Blackwell <gblackwell@startadevelopment.com>
2022-12-28 13:14:25 -0500

    Update IoT-LevelUp-Resource-Provisioning.sh

--- IoT-LevelUp-Resource-Provisioning.sh
+++ IoT-LevelUp-Resource-Provisioning.sh
@@ -1,5 +1,5 @@
 # IoT Level-Up Resource Provisioning
 
-echo Enter a unique Resource Group name:
+echo "Enter a unique Resource Group name:"
 read rg-Name
-echo Your rg name is $rg-Name
+echo "Your rg name is " $rg-Name
